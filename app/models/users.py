79f755cbd064c8eea5692ca4d84c1fb41a4b9bd1
Aida Perez <aida.capeca@gmail.com>
2026-02-27 13:45:04 +0100

    Reorganise models in a separate folder and files

---
+++ app/models/users.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from app.db import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    sex = Column(String, nullable=True)
