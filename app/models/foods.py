9f82e15a1d6eb7e68aaa71f26ae7a3a0eb15c771
Aida Perez <36570763+aidacapeca@users.noreply.github.com>
2026-04-09 12:04:36 +0200

    DB: Modify food model attributes for serving information
    Updated food model to include serving details and changed calories attribute.

--- app/models/foods.py
+++ app/models/foods.py
@@ -8,5 +8,8 @@ class Food(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4())
     name = Column(String, nullable=False)
-    calories_per_100g = Column(Float, nullable=False)
+    calories_per_unit = Column(Float, nullable=False)
+    proteins_per_unit = Column(Float, nullable=False)
+    serving_unit = Column(String, nullable=False) 
+    serving_size = Column(String, nullable=True)
     category = Column(String, nullable=True)
